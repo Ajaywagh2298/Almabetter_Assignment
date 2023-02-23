b9fb6dc59a4e914e250978d34d77accf1fb866a2
ajaywagh_2298 <ajaywagh2298@gmail.com>
2023-02-23 19:40:01 +0530

    add three assignment

--- assignment11.js
+++ assignment11.js
@@ -53,8 +53,8 @@ console.log("Number of Vowels");
 const vowel_count = (str) => {
     let count = 0;
     let vowels = 'aeiouAEIOU';
-    for(let i = 0; i < str.length; i++) {
-        if(vowels.indexOf(str[i]) !== -1) {
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.indexOf(str[i]) !== -1) {
             count++;
         }
     }
